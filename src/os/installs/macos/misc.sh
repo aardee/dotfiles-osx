e7073e266e6750eb02a45f8b6bea19644a21d2b9
Rajan Desai <rajan.desai@hqo.co>
2022-11-23 19:23:55 -0500

    Added required tools

--- src/os/installs/macos/misc.sh
+++ src/os/installs/macos/misc.sh
@@ -8,24 +8,27 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Miscellaneous\n\n"
 
-brew_install "Android File Transfer" "android-file-transfer" "--cask"
+#Mac App Store command-line interface
+brew install mas
+
+#Work with ID3 metadata in .mp3 files
 brew_install "eyeD3" "eye-d3"
-brew_install "Rectangle" "rectangle" "--cask"
-brew_install "Unarchiver" "the-unarchiver" "--cask"
-brew_install "VLC" "vlc" "--cask"
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-#brew_install "coreutils" "coreutils"
+brew_install "coreutils" "coreutils"
 
 # Install some other useful utilities like `sponge`.
-#brew_install "moreutils" "moreutils"
+brew_install "moreutils" "moreutils"
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew_install "findutils" "findutils"
 
 # Install `wget` with IRI support.
 brew_install "wget --with-iri" "wget --with-iri"
+
+#User-friendly command-line shell for UNIX-like operating systems
+brew_install "Fish" "fish"
 
 brew_install "grep" "grep"
 brew_install "openssh" "openssh"
@@ -35,3 +38,17 @@ brew_install "gnu-which --default-names" "gnu-which --default-names"
 brew_install "nmap" "nmap"
 brew_install "rename" "rename"
 brew_install "tree" "tree"
+brew_install "Docker" "docker"
+brew_install "Terraform" "terraform"
+brew_install "jq" "jq"
+
+#Unpacks archive files - https://theunarchiver.com/
+brew_install "Unarchiver" "the-unarchiver" "--cask"
+
+brew_install "VLC" "vlc" "--cask"
+
+if [ -d "$HOME/.nvm" ]; then
+    brew_install "Yarn" "yarn"
+fi
+
+
--- src/os/installs/macos/misc_tools.sh
+++ src/os/installs/macos/misc_tools.sh
@@ -9,19 +9,58 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
-#brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
-brew_install "Slack" "slack" "caskroom/cask" "cask"
-brew_install "1password" "1password" "caskroom/versions" "cask"
-brew_install "dropbox" "dropbox" "caskroom/versions" "cask"
-brew_install "evernote" "evernote" "caskroom/versions" "cask"
-brew_install "devonthink-pro-office" "devonthink-pro-office" "caskroom/versions" "cask"
-brew_install "scansnap-manager-ix500" "fujitsu-scansnap-manager-ix500" "caskroom/drivers" "cask"
-brew_install "hazel" "hazel" "caskroom/versions" "cask"
-brew_install "CyberDuck File Transfer App" "cyberduck" "caskroom/cask" "cask"
 
 #Improved top (interactive process viewer) for OS X (https://github.com/max-horvath/htop-osx)
 brew_install "htop-osx" "htop-osx"
 
-if [ -d "$HOME/.nvm" ]; then
-    brew_install "Yarn" "yarn"
-fi
+brew_install "awscli" "awscli"
+
+brew_install "Slack" "slack" "--cask"
+brew_install "1password" "1password" "--cask"
+brew_install "dropbox" "dropbox" "--cask"
+brew_install "evernote" "evernote" "--cask"
+brew_install "DEVONthink" "devonthink" "--cask"
+brew_install "Scansnap Manager ix500" "fujitsu-scansnap-manager-ix500" "--cask"
+brew_install "Hazel" "hazel" "--cask"
+brew_install "CyberDuck File Transfer App" "cyberduck" "--cask"
+
+brew_install "Miro" "miro" "--cask"
+brew_install "Notion" "notion" "--cask"
+brew_install "Skitch" "skitch" "--cask"
+
+#Android File Transfer
+brew_install "Android File Transfer" "android-file-transfer" "--cask"
+
+#Application launcher and productivity software
+brew_install "Alfred" "alfred" "--cask"
+
+#Tool to hide status bar icons
+brew_install "Dozer" "dozer" "--cask"
+
+#TO-DO List manager
+brew_install "TODOist" "todoist" "--cask"
+
+#iTerm2
+brew_install "iTerm2" "iterm2" "--cask"
+
+brew_install "Grammarly" "grammarly" "--cask"
+
+#Animated screen capture application
+brew_install "LICEcap" "licecap" "--cask"
+
+#Tool to list all active shortcuts of the current application
+brew_install "cheatsheet" "cheatsheet" "--cask"
+
+#Notetaking tool
+brew_install "WorkFlowy" "workflowy" "--cask"
+
+brew_install "spotify" "spotify" "--cask"
+brew_install "Postman" "postman" "--cask"
+brew_install "Textmate Editor" "textmate" "--cask"
+brew_install "Sublime Text Editor" "sublime-text" "--cask"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install MAS apps
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Magnet
+mas install 441258766
